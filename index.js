5d89d5a5aefecf888d20d935bd1d6f20be112fe7
JochemTheLegend27 <51921602+JochemTheLegend27@users.noreply.github.com>
2019-12-26 16:40:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PREFIX = '$';
 
 const fs = require('fs');
 
-
+var stats = JSON.parse(fs.readFileSync('opslag/stats.json', 'utf8'));
 
 
 bot.on('ready', () => {
@@ -21,6 +21,20 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
 var owner = '398441594094616578';
+var gebruiker = msg.guild.id + msg.author.id;
+if(!stats[gebruiker])stats[botid] = {
+    berichten: 0
+  }
+fs.writeFile('opslag/stats.json', JSON.stringify(stats),(err) => {
+    if(err) console.error(err);
+    })
+stats[gebruiker].berichten = stats[gebruiker].berichten + 1;
+fs.writeFile('opslag/stats.json', JSON.stringify(stats),(err) => {
+    if(err) console.error(err);
+    })
+if(msg.content === PREFIX + 'stats'){
+    msg.channel.sendMessage('jij hebt **' + stats[gebruiker].berichten + '** berichten')
+}
 
 
 
